ecaef347502c57982e9c6b178b792a631ae566d4
Dilip Roshitha <dilip.roshitha@grubtech.com>
2023-09-24 10:23:15 +0530

    part 1: added missing getters and setters to model

--- src/main/java/com/dilipblog/simpleframework/part1/application/model/Customer.java
+++ src/main/java/com/dilipblog/simpleframework/part1/application/model/Customer.java
@@ -6,4 +6,28 @@ public class Customer {
   private UUID id;
   private String customerName;
   private String emailAddress;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getCustomerName() {
+    return customerName;
+  }
+
+  public void setCustomerName(String customerName) {
+    this.customerName = customerName;
+  }
+
+  public String getEmailAddress() {
+    return emailAddress;
+  }
+
+  public void setEmailAddress(String emailAddress) {
+    this.emailAddress = emailAddress;
+  }
 }
